b13c4d82a86b47efeeb8a1b5de43e4a5087958fe
OhNoSpirit <ohnospirit@gmail.com>
2022-08-23 19:21:19 -0300

    A

---
+++ Proyecto Final - Coderhouse/Assets/_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
